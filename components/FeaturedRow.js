df67339a9786d31ae409ba7bbdc955864afceab2
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-24 12:03:20 -0400

    testing restaurant card

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -30,6 +30,42 @@ const FeaturedRow = ({ id, title, description }) => {
             long={20}
             lat={0}
         />
+        <RestaurantCards 
+            id={123}
+            imageUrl='https://links.papareact.com/gn7'
+            title='Yo! Sushi'
+            rating={4.5}
+            genre='Japanese'
+            address='123 Main St'
+            short_description='This is a test description'
+            dishes={[]}
+            long={20}
+            lat={0}
+        />
+        <RestaurantCards 
+            id={123}
+            imageUrl='https://links.papareact.com/gn7'
+            title='Yo! Sushi'
+            rating={4.5}
+            genre='Japanese'
+            address='123 Main St'
+            short_description='This is a test description'
+            dishes={[]}
+            long={20}
+            lat={0}
+        />
+        <RestaurantCards 
+            id={123}
+            imageUrl='https://links.papareact.com/gn7'
+            title='Yo! Sushi'
+            rating={4.5}
+            genre='Japanese'
+            address='123 Main St'
+            short_description='This is a test description'
+            dishes={[]}
+            long={20}
+            lat={0}
+        />
       </ScrollView>
     </View>
   )
