10f18b7f3835aedf5096464d6e5a2014f1bbfa61
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-24 10:46:39 -0400

    silly padding

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -22,7 +22,7 @@ const HomeScreen = () => {
     return (
     <SafeAreaView className='bg-white pt-5'>
         {/* Header */}
-        <View className='flex-row pb-3 items-center mx-4 space-x-2 px-4'>
+        <View className='flex-row pb-3 items-center mx-4 space-x-2'>
             <Image 
                 source={{uri: 'https://links.papareact.com/wru'}}
                 className='h-7 w-7 bg-gray-300 p-4 rounded-full'
@@ -38,7 +38,7 @@ const HomeScreen = () => {
         </View>
 
         {/* Search */}
-        <View className='flex-row items-center space-x-2 pb-2 mx-4 px-4'>
+        <View className='flex-row items-center space-x-2 pb-2 mx-4'>
             <View className='flex-row space-x-2 bg-gray-200 p-3 flex-1'>
                 <MagnifyingGlassIcon color={'gray'} size={20}/>
                 <TextInput 
