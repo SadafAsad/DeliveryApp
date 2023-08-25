ce5e6c14837bb37d586ee6a4b626c11e6ca832f8
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-25 18:26:14 -0400

    restaurant in global store

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,14 +1,18 @@
 import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import React, { useEffect, useLayoutEffect } from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import { urlFor } from '../sanity'
 import { ArrowLeftIcon, QuestionMarkCircleIcon } from 'react-native-heroicons/outline'
 import { StarIcon, MapPinIcon, ChevronRightIcon } from 'react-native-heroicons/solid'
 import DishRow from '../components/DishRow'
 import BasketIcon from '../components/BasketIcon'
+import { useDispatch } from 'react-redux'
+import { setRestaurant } from '../slices/restaurantSlice'
 
 const RestaurantScreen = () => {
     const navigation = useNavigation()
+    const dispatch = useDispatch()
+
     const {params: {
         id, 
         imageUrl, 
@@ -21,6 +25,21 @@ const RestaurantScreen = () => {
         long,
         lat
     }} = useRoute()
+
+    useEffect(() => {
+        dispatch(setRestaurant({
+            id, 
+            imageUrl, 
+            title,
+            rating,
+            genre,
+            address,
+            short_description,
+            dishes,
+            long,
+            lat
+        }))
+    }, [dispatch])
 
     useLayoutEffect(() => {
         navigation.setOptions({
