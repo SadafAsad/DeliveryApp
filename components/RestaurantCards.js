993311627e21fec47b81c48b9e24e74b7fa185ac
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-25 14:41:39 -0400

    restaurant header

--- components/RestaurantCards.js
+++ components/RestaurantCards.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { StarIcon } from 'react-native-heroicons/solid'
 import { MapPinIcon } from 'react-native-heroicons/outline'
 import { urlFor } from '../sanity'
+import { useNavigation } from '@react-navigation/native'
 
 const RestaurantCards = ({ 
     id, 
@@ -16,8 +17,27 @@ const RestaurantCards = ({
     long,
     lat
 }) => {
+
+  const navigation = useNavigation()
+
   return (
-    <TouchableOpacity className='bg-white mr-3 shadow'>
+    <TouchableOpacity 
+      onPress={() => {
+        navigation.navigate('Restaurant', {
+          id, 
+          imageUrl, 
+          title,
+          rating,
+          genre,
+          address,
+          short_description,
+          dishes,
+          long,
+          lat
+        })
+      }}
+      className='bg-white mr-3 shadow'
+    >
       <Image 
         source={{uri: urlFor(imageUrl).url()}}
         className='h-36 w-64 rounded-sm'
