7e3b75ab83ad725313db2057f4f97ca0aacfb685
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-25 20:31:09 -0400

    preparing screen layout

---
+++ screens/PreparingScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PreparingScreen = () => {
+  return (
+    <View>
+      <Text>PreparingScreen</Text>
+    </View>
+  )
+}
+
+export default PreparingScreen
