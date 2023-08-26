9a823b9af197a4d8b9526d732e212d66d68a8f7d
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-25 20:42:16 -0400

    Preparing Screen BEAUTIFUL

--- screens/PreparingScreen.js
+++ screens/PreparingScreen.js
@@ -1,10 +1,16 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
+import * as Animatable from 'react-native-animatable'
 
 const PreparingScreen = () => {
   return (
     <SafeAreaView className='bg-[#00ccbb] flex-1 justify-center items-center'>
-      <Text>PreparingScreen</Text>
+      <Animatable.Image
+        source={require('../assets/orderLoading.gif')}
+        animation='slideInUp'
+        iterationCount={1}
+        className='h-96 w-96'
+      />
     </SafeAreaView>
   )
 }
