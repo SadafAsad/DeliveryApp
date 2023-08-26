40768d91a050362f5540394c2bc7608dead29fd1
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-25 21:31:26 -0400

    DONE

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -58,6 +58,18 @@ const DeliveryScreen = () => {
                     pinColor='#00ccbb'
                 />
             </MapView>
+
+            <SafeAreaView className='bg-white flex-row items-center space-x-5 h-28'>
+                <Image
+                    source={{uri: 'https://links.papareact.com/wru'}}
+                    className='h-12 w-12 bg-gray-300 rounded-full ml-5'
+                />
+                <View className='flex-1'>
+                    <Text className='text-lg'>Sadaf Asad</Text>
+                    <Text className='text-gray-400'>Your Rider</Text>
+                </View>
+                <Text className='text-[#00ccbb] text-lg mr-5 font-bold'>Call</Text>
+            </SafeAreaView>
         </View>
     )
 }
